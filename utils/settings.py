45a5f8bc3fa4ed2c975d434a8eb9f36604e6121d
ypeiyu <yangpeiyv@gmail.com>
2023-06-16 22:11:59 +0800

    first commit

--- utils/settings.py
+++ utils/settings.py
@@ -1,1 +1,16 @@
 img_size = 224
+
+parser_choices = {
+    'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'AGI', 'LPI', 'Random'],
+    'model': ['resnet34', 'vgg16'],
+    'dataset': ['ImageNet'],
+}
+
+parser_default = {
+    'attr_method': 'LPI',
+    'model': 'resnet34',
+    'dataset': 'ImageNet',
+    'k': 1,
+    'bg_size': 20,
+    'num_centers': 11,
+}
