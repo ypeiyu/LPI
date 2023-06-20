d5db5bd7c67928bc5e34077b9344d24974a52c45
ypeiyu <yangpeiyv@gmail.com>
2023-06-20 18:20:08 +0800

    update evaluator

--- evaluator.py
+++ evaluator.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 import os
 
 from utils import undo_preprocess_input_function
-from exp_fig import visualize
+from utils import visualize
 
 
 def normalize_saliency_map(saliency_map):
-    saliency_map = torch.sum(torch.abs(saliency_map), dim=1, keepdim=True)
+    # saliency_map = torch.sum(torch.abs(saliency_map), dim=1, keepdim=True)
+    saliency_map = torch.abs(saliency_map)
 
     flat_s = saliency_map.view((saliency_map.size(0), -1))
     temp, _ = flat_s.min(1, keepdim=True)
@@ -19,6 +20,7 @@ def normalize_saliency_map(saliency_map):
     flat_s = saliency_map.view((saliency_map.size(0), -1))
     temp, _ = flat_s.max(1, keepdim=True)
     saliency_map = saliency_map / (temp.unsqueeze(1).unsqueeze(1) + 1e-10)
+
     return saliency_map
 
 
@@ -37,82 +39,90 @@ class Evaluator(object):
         self.NL_difference = []
         self.model.eval()
 
-    def DiffID(self, q_ratio_lst, centers=None):
+    def DiffID(self, ratio_lst, centers=None):
         log = self.log
         self.n_examples = 0
         self.n_correct = 0
-        self.pos_num = 0
-        n_pert_correct_top_lst = [0 for _ in range(len(q_ratio_lst))]
-        n_pert_correct_bot_lst = [0 for _ in range(len(q_ratio_lst))]
-        start_loc_top = [0 for _ in range(len(q_ratio_lst))]
-        start_loc_bot = [0 for _ in range(len(q_ratio_lst))]
-        logit_change_top_lst = [torch.zeros(len(self.dataloader.dataset.samples)) for _ in range(len(q_ratio_lst))]
-        logit_change_bot_lst = [torch.zeros(len(self.dataloader.dataset.samples)) for _ in range(len(q_ratio_lst))]
+
+        loc_ind = 0
+        ratio_len = len(ratio_lst)
+
+        n_pert_correct_del_ins_lst = [[0 for _ in range(ratio_len)], [0 for _ in range(ratio_len)]]
+        logit_change_del_ins_lst = [[torch.zeros(len(self.dataloader.dataset.samples)) for _ in range(ratio_len)],
+                                    [torch.zeros(len(self.dataloader.dataset.samples)) for _ in range(ratio_len)]]
 
         start = time.time()
-        for batch_num, (image, label) in enumerate(self.dataloader):
-            image = image.cuda()
+        for batch_num, (batch_image, label) in enumerate(self.dataloader):
+            batch_image = batch_image.cuda()
             target = label.cuda()
 
-            batch_size = image.shape[0]
+            batch_size = batch_image.shape[0]
 
-            output = self.model(image).detach()
+            output = self.model(batch_image).detach()
             _, predicted = torch.max(output.data, 1)
             self.n_correct += (predicted == target).sum().item()
             self.n_examples += batch_size
 
+            # ------------------ attribution estimation -------------------------
             if centers is not None:
-                # ------------------ LPI -------------------------
                 output_array = output.cpu().numpy()
                 clu_lst = [np.argmin(np.linalg.norm(centers - output_array[bth], axis=1)) for bth in range(output.shape[0])]
-                saliency_map = self.explainer.shap_values(image, sparse_labels=target, centers=clu_lst)
+                saliency_map = self.explainer.shap_values(batch_image, sparse_labels=target, centers=clu_lst)
             else:
-                saliency_map = self.explainer.shap_values(image, sparse_labels=target)
+                saliency_map = self.explainer.shap_values(batch_image, sparse_labels=target)
 
             # -------------------------------- saliency map normalization -----------------------------------------
             saliency_map = normalize_saliency_map(saliency_map.detach())
 
             self.model.eval()
-            zero_tensor = torch.zeros(*image[0].shape).cuda()
-            perturb_img_batch = torch.zeros(*image.shape).cuda()
-            for q_ind, q_ratio in enumerate(q_ratio_lst):
-                # ========================================================================
-                for perturb_top in [False, True]:
+            perturb_img_batch = torch.zeros(*batch_image.shape).cuda()
+            num_elements = batch_image[0].numel()
 
-                    q_r = 1-q_ratio if perturb_top else q_ratio  # is_top 0.9 else 0.1
-                    threshold = torch.quantile(saliency_map.reshape(saliency_map.shape[0], -1), q=q_r, dim=1, interpolation='midpoint')  # < top90
+            for r_ind, ratio in enumerate(ratio_lst):
+                for deletion in [False, True]:
+                    del_ratio = ratio if deletion else 1-ratio # if del: pre_r=1-0.1=0.9 else ins: pre=0.1     if del: s_map(des=ture) else ins: s_map(des=false)[:]
 
                     for b_num in range(batch_size):
-                        sat = image.detach()[b_num] if perturb_top else zero_tensor
-                        dis_sat = zero_tensor if perturb_top else image.detach()[b_num]
-
-                        perturb = torch.where(saliency_map[b_num] < threshold[b_num], sat, dis_sat).unsqueeze(0)
-                        mean_insertion = torch.sum(perturb) / (torch.count_nonzero(perturb) + 1e-10)
-                        dis_sat_t = dis_sat+mean_insertion if perturb_top else dis_sat
-                        sat_t = sat if perturb_top else sat+mean_insertion
-                        perturb_img = torch.where(saliency_map[b_num] < threshold[b_num], sat_t, dis_sat_t).unsqueeze(0)
-
-                        # flag = 'insertion' if perturb_top else 'deletion'
-                        # single_img_inspection(perturb_img, file_name='exp_fig/img_inspection/img' + str(
-                        #     self.n_examples + b_num) + '_' + flag + str(q_r) + '.jpg')
+                        image = batch_image.detach()[b_num]
+                        flat_s_map = saliency_map[b_num].view(-1)
+                        flat_image = image.view(-1)
+                        # order by attributions
+                        sorted_ind = torch.argsort(flat_s_map, descending=deletion)
+                        # preserve pixels
+                        num_delete = int(num_elements * del_ratio)
+                        preserve_ind = sorted_ind[num_delete:]
+                        mask = torch.zeros_like(flat_image)
+                        mask[preserve_ind] = 1
+                        mean_preserve = torch.mean(flat_image[preserve_ind])
+                        perturb_img = flat_image * mask + (1-mask) * mean_preserve
+                        perturb_img = perturb_img.view(image.size())
 
                         perturb_img_batch[b_num] = perturb_img
 
                     output_pert = self.model(perturb_img_batch).detach()
 
                     _, predicted_pert = torch.max(output_pert.data, 1)
-                    if perturb_top:
-                        n_pert_correct_top_lst[q_ind] += (predicted_pert == target).sum().item()
-                        for bth in range(batch_size):
-                            t = target[bth]
-                            logit_change_top_lst[q_ind][start_loc_top[q_ind]:(start_loc_top[q_ind]+1)] = output_pert[bth, t]/output[bth, t]
-                            start_loc_top[q_ind] += 1
-                    else:
-                        n_pert_correct_bot_lst[q_ind] += (predicted_pert == target).sum().item()
-                        for bth in range(batch_size):
-                            t = target[bth]
-                            logit_change_bot_lst[q_ind][start_loc_bot[q_ind]:(start_loc_bot[q_ind]+1)] = output_pert[bth, t]/output[bth, t]
-                            start_loc_bot[q_ind] += 1
+
+                    l_i = 0 if deletion else 1
+                    n_pert_correct_del_ins_lst[l_i][r_ind] += (predicted_pert == target).sum().item()
+                    for bth in range(batch_size):
+                        t = target[bth]
+                        logit_change_del_ins_lst[l_i][r_ind][loc_ind+bth:loc_ind+bth+1] = output_pert[bth, t] / output[bth, t]
+                        # loc_ind += 1
+
+            # deletion logit scores
+            # [0.67, 0.543, 0.453, 0.364, 0.277, 0.19, 0.128, 0.057, -0.008]
+            # deletion accu scores
+            # [0.428, 0.297, 0.197, 0.125, 0.08, 0.049, 0.025, 0.013, 0.005]
+            #
+            #
+            # insertion logit scores
+            # [0.019, 0.129, 0.24, 0.345, 0.444, 0.546, 0.633, 0.716, 0.806]
+            # insertion accu scores
+            # [0.006, 0.03, 0.08, 0.141, 0.22, 0.308, 0.395, 0.496, 0.587]
+
+            loc_ind += batch_size
+
         end = time.time()
         log('\ttime: \t{:.3f}'.format(end - start))
         insertion_logit = []
@@ -124,29 +134,25 @@ class Evaluator(object):
         DiffID_logit = []
         DiffID_acc = []
 
-        for q_ind, q_ratio in enumerate(q_ratio_lst):
-            # ========================================================================
-            mean_accu_top = n_pert_correct_top_lst[q_ind]/self.n_examples
-            var_top, mean_top = torch.var_mean(logit_change_top_lst[q_ind], unbiased=False)
-            mean_top = mean_top.item()
-            deletion_logit.append(round(mean_top, 3))
-            deletion_acc.append(round(mean_accu_top, 3))
+        for r_ind in range(ratio_len):
+            mean_accu_del = n_pert_correct_del_ins_lst[0][r_ind] / self.n_examples
+            var_del, mean_del = torch.var_mean(logit_change_del_ins_lst[0][r_ind], unbiased=False)
+            mean_del = mean_del.item()
+            deletion_logit.append(round(mean_del, 3))
+            deletion_acc.append(round(mean_accu_del, 3))
 
-            mean_accu_bot = n_pert_correct_bot_lst[q_ind]/self.n_examples
-            var_bot, mean_bot = torch.var_mean(logit_change_bot_lst[q_ind], unbiased=False)
-            mean_bot = mean_bot.item()
-            insertion_logit.append(round(mean_bot, 3))
-            insertion_acc.append(round(mean_accu_bot, 3))
+            mean_accu_ins = n_pert_correct_del_ins_lst[1][-(r_ind+1)] / self.n_examples
+            var_ins, mean_ins = torch.var_mean(logit_change_del_ins_lst[1][-(r_ind+1)], unbiased=False)
+            mean_ins = mean_ins.item()
+            insertion_logit.append(round(mean_ins, 3))
+            insertion_acc.append(round(mean_accu_ins, 3))
 
-            # log('\tDiff criterion')
-            del_accu = mean_accu_bot - mean_accu_top
-            del_logit = mean_bot - mean_top
+            del_accu = mean_accu_ins - mean_accu_del
+            del_logit = mean_ins - mean_del
             DiffID_logit.append(round(del_logit, 3))
             DiffID_acc.append(round(del_accu, 3))
-        self.log('deletion 10-90 logit scores')
+        self.log('deletion logit scores')
         self.log(deletion_logit)
-        # print(np.mean(np.array(deletion_logit)))
-
         self.log('deletion accu scores')
         self.log(deletion_acc)
         self.log('\n')
@@ -159,3 +165,29 @@ class Evaluator(object):
         self.log(DiffID_logit)
         self.log('Diff accu scores')
         self.log(DiffID_acc)
+
+    def visual_inspection(self, file_name, num_vis, method_name, centers=None):
+        for batch_num, (image, label) in enumerate(self.dataloader):
+            if (batch_num * image.shape[0]) >= num_vis:
+                break
+            image = image.cuda()
+            target = label.cuda()
+
+            if centers is not None:
+                # ---------------- LPI ---------------------
+                output = self.model(image).detach()
+                output_array = output.cpu().numpy()
+                clu_lst = [np.argmin(np.linalg.norm(centers - output_array[bth], axis=1)) for bth in
+                           range(output.shape[0])]
+                saliency_map = self.explainer.shap_values(image, sparse_labels=target, centers=clu_lst)
+            else:
+                saliency_map = self.explainer.shap_values(image, sparse_labels=target)
+
+            image = undo_preprocess_input_function(image).detach().cpu().numpy()
+
+            if not os.path.exists(file_name):
+                os.mkdir(file_name)
+            for bth in range(image.shape[0]):
+                img = image[bth]
+                f_name = file_name + 'img_' + str(int(batch_num * self.dataloader.batch_size) + bth)
+                visualize(image=img, saliency_map=saliency_map[bth], filename=f_name, method_name=method_name)
