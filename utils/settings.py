033faed703f19dbc578ebb79ea2193ce5d56c4a4
ypeiyu <yangpeiyv@gmail.com>
2023-06-30 17:41:50 +0800

    update settings

--- utils/settings.py
+++ utils/settings.py
@@ -3,14 +3,14 @@ img_size = 224
 parser_choices = {
     'attr_method': ['InputGrad', 'IntGrad', 'ExpGrad', 'AGI', 'LPI', 'Random'],
     'model': ['resnet34', 'vgg16'],
-    'dataset': ['ImageNet'],
+    'dataset': ['imagenet'],
     'metric': ['DiffID', 'visualize'],
 }
 
 parser_default = {
     'attr_method': 'LPI',
     'model': 'resnet34',
-    'dataset': 'ImageNet',
+    'dataset': 'imagenet',
     'metric': 'DiffID',
     'k': 5,
     'bg_size': 20,
